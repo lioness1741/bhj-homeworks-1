2e82742a01fa34781d36ad03d1870b756de686f9
Tatyana <lioness1741@mail.ru>
2023-03-28 23:11:41 +0600

    Доработано решение cookie-clicker и mole-game

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -3,20 +3,21 @@ const speed = document.getElementById("clicker__speed");
 const cookie = document.getElementById("cookie");
 const clickTime = Date.now();
 
+
 cookie.addEventListener("click", (element) => {
   const maxCount = parseInt(clickerCounter.textContent);
   clickerCounter.textContent = maxCount + 1;
-  speed.textContent = (1000 / (Date.now() - clickTime)).toFixed(2);
-  clickTime = Date.now();
   if (maxCount % 2 === 0) {
     cookie.width = 300;
   } else {
     cookie.width = 200;
   }
+  speed.textContent = (1000 / (Date.now() - clickTime)).toFixed(2);
+  clickTime = Date.now();  
 });
 
 
-/* 
+/*
 const cookie = document.getElementById("cookie");
 const clickerCounter = document.getElementById("clicker__counter");
 cookie.onclick = () => {
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -8,18 +8,18 @@ for (let index = 1; index <= 9; index++) {
   }
   let hole = getHole(index);
 
-  hole.onclick = function () {
+   hole.onclick = function () {
     if (hole.className.includes("hole_has-mole") === true) {
       dead.innerHTML++;
     } else {
       lost.innerHTML++;
     }
-    if (dead.textContent == 10) {
-      alert("Вы выиграли!");
-      dead.innerHTML = 0;
-      lost.innerHTML = 0;
-    } else if (lost.textContent == 5) {
-      alert("Вы проиграли!");
+    function alertOn() {
+      if (dead.textContent == 10) {
+        alert("Вы выиграли!");
+      } else if (lost.textContent == 5) {
+        alert("Вы проиграли!");
+      }
       dead.innerHTML = 0;
       lost.innerHTML = 0;
     }
