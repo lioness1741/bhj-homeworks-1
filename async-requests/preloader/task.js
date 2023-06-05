a80b5571a2665a0dd574dad3116c0455b093a7c2
Tatyana <lioness1741@mail.ru>
2023-06-05 22:27:17 +0600

    Задание Асинхронные запросы доработано

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,33 +1,48 @@
 const loader = document.getElementById("loader");
 const requestOnDesk = document.getElementById("items");
+const xhr = new XMLHttpRequest();
+let localValute = JSON.parse(localStorage.getItem("localValute"));
 
-let xhr = new XMLHttpRequest();
-
-xhr.open("GET", "https://students.netoservices.ru/nestjs-backend/slow-get-courses");
+xhr.open(
+  "GET",
+  "https://students.netoservices.ru/nestjs-backend/slow-get-courses"
+);
 xhr.setRequestHeader("Content-Type", "application/json");
-xhr.addEventListener("readystatechange", (event) => {
+xhr.addEventListener("load", (event) => {
+  
+   if (xhr.readyState === xhr.OPENED) {
+        if (localValute != null) {
+            loader.classList.remove("loader_active");
+            addValute(localValute);
+        }
+    }
+  
   if (xhr.readyState === xhr.DONE) {
     loader.classList.remove("loader_active");
-    
-      const responseServer = JSON.parse(xhr.responseText).response.Valute;
-
-        for (const valute in responseServer) {
-            const item = `<div class="item">
-                            <div class="item__code">
-                                ${responseServer[valute].CharCode}
-                            </div>
-                            <div class="item__value">
-                                ${responseServer[valute].Value}
-                            </div>
-                            <div class="item__currency">
-                                руб.
-                            </div>
-                          </div>`;
-
-            requestOnDesk.insertAdjacentHTML('beforeend', item);
-        }
+    requestOnDesk.innerHTML = "";
+    const responseServer = JSON.parse(xhr.responseText).response.Valute;
+    addValute(responseServer);
   }
 });
 
+function addValute(responseServer) {
+  for (let valute in responseServer) {
+    let item = document.createElement("div");
+    item.classList.add("item");
+    requestOnDesk.appendChild(item);
+    item.insertAdjacentHTML(
+      "beforeBegin",
+      `<div class="item">
+                    <div class="item__code">
+                      ${responseServer[valute].CharCode}</div>
+                    <div class="item__value">${responseServer[valute].Value}</div>
+                    <div class="item__currency">руб.</div>
+                 </div>`
+    );
+  }
+}
 xhr.send();
 
+function updateList (responseServer) {
+    localStorage.setItem("localValute", JSON.stringify(responseServer));
+} 
